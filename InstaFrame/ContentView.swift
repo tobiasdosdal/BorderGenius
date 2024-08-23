663e294a0f064aabb0362ac1f94af51f7db3d402
Tobias Dosdal-Feddersen <kontakt@tobiasdosdal.dk>
2024-08-23 09:42:52 +0200

    Initial Commit

---
+++ InstaFrame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaFrame
+//
+//  Created by Tobias Dosdal-feddersen on 23/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaFrame/InstaFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFrameApp.swift
+//  InstaFrame
+//
+//  Created by Tobias Dosdal-feddersen on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
